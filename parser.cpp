c04b89410c89be6fca790477c37c3422fd9227c3
Evelyn Lu <evelyn.lu099@gmail.com>
2018-12-09 10:43:31 -0800

    redesign parser class

--- parser.cpp
+++ parser.cpp
@@ -1,49 +1,54 @@
 #include "parser.h"
 
 //PUBLIC
-Parser::Parser() {//init all variables
 
+void Parser::test() {
+
+}
+
+
+Parser::Parser() {//init all variables
+    setUserIn("");
 }
 
 Parser::Parser(string _userIn) {//init with userInput & convert
-
+    setUserIn(_userIn);
 }
 
 Parser::~Parser() {
-
+    setUserIn("");
 }
 
 void Parser::setUserIn(string _userIn) {
-
+    userIn = _userIn;
 }
 
-void Parser::process() { //convert userIn to rpn string
-
+void Parser::process() {
+    /* convert userIn to rpn string using shunting yard algorithm
+     * from userIn
+     * to rpn
+     */
 }
 
 string Parser::getUserIn() const {
-
+    return userIn;
 }
 
 string Parser::getRpn() const {
+    return rpn;
+}
+
+Parser::tokenType Parser::getToken(string &str, string &token) {
+    /* extract a token from the beginning of str
+     * e.g. for str = "3 3/4+5-7.3+8 2/3*5/6/3 5/6/3.2"
+     * return NUMBER
+     * str = "+5-7.3+8 2/3*5/6/3 5/6/3.2"
+     * token = "3 3/4"
+     * e.g. for str = "+5-7.3+8 2/3*5/6/3 5/6/3.2"
+     * return OPERATOR
+     * str = "5-7.3+8 2/3*5/6/3 5/6/3.2"
+     * token = "+"
+     */
 
 }
 
-
-//PRIVATE
-void Parser::initialize() {
-    if(!mixedStk.empty())
-        mixedStk.pop();
-    if(!opStk.empty())
-        opStk.pop();
-
-}
-
-void Parser::splitInput() { //split userIn into mixedStk and opStk
-
-}
-
-void Parser::generateRpn() { //when mixedStk and opStk are done, generate Rpn string
-
-
-}
--- parser.h
+++ parser.h
@@ -3,28 +3,35 @@
 #include "mixednumber.h"
 #include <string>
 #include <stack>
+#include <regex>
 
 using namespace std;
 
 class Parser
 {
 public:
+    void test();
+
     Parser();//init all variables
     Parser(string _userIn);//init with userInput & convert
     ~Parser();
     void setUserIn(string _userIn);
-    void process(); //convert userIn to rpn string
+    void process(); //convert userIn to rpn string using shunting yard algorithm
     string getUserIn() const;
     string getRpn() const;
+
 private:
-    stack<mixedNumber> mixedStk; //mixed number stack
-    stack<char> opStk; //operator stack
-    stack<string> rpnStk; //rpn as a stack.
     string userIn; //user input, e.g. "3 1/2 + 5.6 * 7/9 / 10"
     string rpn; //output rpn, e.g. "3 1/2 5.6 7/9 * 10 / +"
-    void initialize();
-    void splitInput(); //split userIn into mixedStk and opStk
-    void generateRpn(); //when mixedStk and opStk are done, generate Rpn string
+    enum tokenType { //for the shunting yard algorithm
+        NUMBER,
+        FUNCTION,
+        OPERATOR,
+        LEFT_BRACKET,
+        RIGHT_BRACKET,
+        NOTHING
+    };
+    tokenType getToken(string &str, string &token); //extract a token from the beginning of str
 };
 
 #endif // PARSER_H
